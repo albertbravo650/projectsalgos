1cdc6542e10e830173dd08bd018ad16738d15d08
Albert B <abduarte20@gmail.com>
2023-01-13 21:55:10 -0800

    next commit

--- Arrays/practice1.js
+++ Arrays/practice1.js
@@ -1,4 +1,24 @@
-var firstName = "Albert";
-var lastName = "Bravo";
-var age = 26;
-console.log(firstName + " " + lastName);
+// var firstName = "Albert";
+// var lastName = "Bravo";
+// var age = 26;
+// console.log(firstName + " " + lastName);
+
+var myArr1 = [];
+
+myArr1.push(10);
+console.log(myArr1);
+
+myArr1.push(100);
+console.log(myArr1);
+
+myArr1.push(1000);
+console.log(myArr1);
+
+var myString = "Hello!!";
+var someChar = myString.charAt(4);
+console.log(someChar);
+console.log(myString[4]);
+
+for(i=0; i<myArr1.length; i++) {
+    console.log(myArr1[i]);
+}
