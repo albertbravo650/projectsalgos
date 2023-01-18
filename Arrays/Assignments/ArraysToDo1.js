775253eaa29f76bcb7f1afa804eeaf2d7750b210
Albert B <abduarte20@gmail.com>
2023-01-18 00:33:38 -0800

    next commit

--- Arrays/Assignments/ArraysToDo1.js
+++ Arrays/Assignments/ArraysToDo1.js
@@ -1,26 +1,26 @@
 // Push Front
-function pushFront(arr, value) {
-    for(var i=arr.length-1; i>=0; i--) {
-        arr[i+1] = arr[i];
-    }
-    arr[0] = value;
-    console.log(arr);
-}
+// function pushFront(arr, value) {
+//     for(var i=arr.length-1; i>=0; i--) {
+//         arr[i+1] = arr[i];
+//     }
+//     arr[0] = value;
+//     console.log(arr);
+// }
 
-var arr1 = [5,7,2,3];
-pushFront(arr1, 8);
-var arr2 = [99];
-pushFront(arr2, 7);
+// var arr1 = [5,7,2,3];
+// pushFront(arr1, 8);
+// var arr2 = [99];
+// pushFront(arr2, 7);
 
 
 // Pop Front
 // function popFront(arr) {
 //     var value1 = arr[0];
-//     var newArr = [];
-//     for(var i=1; i<arr.length; i++) {
-//         newArr.push(arr[i]);
+//     for(var i=0; i<arr.length; i++) {
+//         arr[i] = arr[i+1];
 //     }
-//     console.log(newArr);
+//     arr.pop();
+//     console.log(arr);
 //     console.log(value1);
 //     return value1;
 // }
@@ -44,7 +44,15 @@ pushFront(arr2, 7);
 //     console.log(newArr);
 // }
 
-// var arr5 = [100,200,5];
-// insertAt(arr5, 2, 311);
-// var arr6 = [9,33,7];
-// insertAt(arr6, 1, 42);
+function insertAt(arr, index, value) {
+    for(var i=arr.length-1; i>=index; i--) {
+        arr[i+1] = arr[i];
+    }
+    arr[index] = value;
+    console.log(arr);
+}
+
+var arr5 = [100,200,5];
+insertAt(arr5, 2, 311);
+var arr6 = [9,33,7];
+insertAt(arr6, 1, 42);
