b7c93f8f8fe7e6a9ec8204d6dfb9cffa69755453
Albert B <abduarte20@gmail.com>
2023-01-25 21:38:27 -0800

    next commit

--- Arrays/practice2.js
+++ Arrays/practice2.js
@@ -1,5 +1,6 @@
 // practice on main branch
 // 2nd commit
+// next commit
 
 var myArr = [42,"hi"]; // myArr.length == 2
 myArr.push(true); // myArr == [42,"hi",true] and length == 3
@@ -7,3 +8,4 @@ myArr[myArr.length+1] = 2; // myArr == [42,"hi",true,undefined,2]
 console.log(myArr.length); // "5", although we set only 4 values
 myArr.pop(); // myArr == [42,"hi",true,undefined]
 console.log(myArr.length); // "4", although we never set myArr[3]
+
