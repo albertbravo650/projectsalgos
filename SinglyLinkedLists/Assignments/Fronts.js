2d57718a3023879aa5944784c1fb80e8fc963868
Albert B <abduarte20@gmail.com>
2023-02-01 18:03:32 -0800

    1st commit

---
+++ SinglyLinkedLists/Assignments/Fronts.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(val) {
+        // creating a new node object with the value provided
+        let new_node = new Node(val);
+        // checking to see if the current list does not have a head node(if the
+        // list is empty)
+        // if list is empty, place new node as head
+        if(!this.head) {
+            this.head = new_node;
+            return this;
+        }
+        // if list is not empty, assign head to be the next node to the new node
+        new_node.next = this.head;
+        // assign new_node to be the new head of the list
+        this.head = new_node;
+        return this;
+    }
+
+    removeFront() {
+        
+    }
+}
+
+SLL1 = new SLL();
+SLL1.addFront(18);
+SLL1.addFront(5);
+SLL1.addFront(73);
+console.log(SLL1)
